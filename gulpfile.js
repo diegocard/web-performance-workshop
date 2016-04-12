e7af21e3c66c48dcea49ad48d515d290c875a21d
Diego Cardozo <diegocard@gmail.com>
2016-04-12 01:55:16 -0300

    Finished step 2

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,33 @@ var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
+var purify = require('gulp-purifycss');
 
 gulp.task('local', function() {
-  connect.server();
+    connect.server();
 });
 
-gulp.task('styles', function() {
-  return gulp.src('stylesheets/*.css')
-    .pipe(concat('styles.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('output'));
+gulp.task('minify-styles', function() {
+    return gulp.src('stylesheets/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('output'));
 });
- 
-gulp.task('scripts', function() {
-  return gulp.src('javascripts/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(uglify())
+
+gulp.task('minify-scripts', function() {
+    return gulp.src('javascripts/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('output'));
+});
+
+gulp.task('unused-css', function() {
+  return gulp.src('stylesheets/*.css')
+    .pipe(purify(['./javascripts/*.js', './*.html']))
     .pipe(gulp.dest('output'));
 });
 
 gulp.task('default', ['local']);
-gulp.task('minify', ['styles', 'scripts']);
+gulp.task('minify', ['minify-styles', 'minify-scripts']);
